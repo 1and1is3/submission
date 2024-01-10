a0acdcb14db9b23a94cb5f3673701ec9b13d3d38
Roland Schneck (FG-6-V-71) <roland.schneck@bmw.de>
2024-01-10 15:07:26 +0100

    add sanitycheck.py

--- test_main.py
+++ test_main.py
@@ -52,3 +52,23 @@ def test_inference_request():
     }
     response = client.post("/inference/", data=json.dumps(data))
     assert response.json() == ('>50k€' or '<50k€')
+
+def test_inference_request_2():
+    data = {
+        "age": 11,
+        "workclass": "Self-emp-inc",
+        "fnlgt": 4,
+        "education": "Bachelors",
+        "education-num": 4,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 1,
+        "native-country": "United-States"
+    }
+    response = client.post("/inference/", data=json.dumps(data))
+    assert response.json() == ('>50k€' or '<50k€')
